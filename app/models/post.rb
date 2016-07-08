1b4e7dc80db90117637b49535c9864457890378a
greatermeans <gakouri@gmail.com>
2016-07-08 15:01:36 -0400

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,6 +3,15 @@ class Post < ActiveRecord::Base
   has_many :comments
   has_many :post_tags
   has_many :tags, :through => :post_tags
-
+  accepts_nested_attributes_for :tags, reject_if: proc {|attributes| attributes[:name].blank?}
   validates_presence_of :name, :content
+
+  # def tags_attributes=(stuff)
+  # 	binding.pry
+  # 	if stuff[0].present?
+  # 		binding.pry
+  # 		stuff.values.each {|tag| self.tags.build(tag) if tag[:name].present?}
+  # 	end
+
+  # end
 end
